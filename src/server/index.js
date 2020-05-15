d3dd337311bb6013ad2ba7b26676651f86a945ed
MohgaEmam <38896263+MohgaEmam@users.noreply.github.com>
2020-05-15 06:49:10 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,58 +1,57 @@
-//requiring environment variables to protect Aylien API ID and key
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-var path = require("path");
-const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
+var path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-var aylien = require("aylien_textapi");
+const aylien = require('aylien_textapi');
 
-// set aylien API credentials
+// Set aylien API credentials
 var textapi = new aylien({
-  application_id: process.env.API_ID,
+    application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
 });
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('dist'));
 
 console.log(__dirname);
 
-app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile("dist/index.html");
-});
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html');
+    // res.sendFile(path.resolve('src/client/views/index.html'))
+})
 
 // designates what port the app will listen to for incoming requests
-app.listen(8085, function () {
-  console.log("Example app listening on port 8085!");
-});
+app.listen(8085, function() {
+    console.log('Example app listening on port 8085!')
+})
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
+app.get('/test', function(req, res) {
+    res.send(mockAPIResponse)
+})
 
-//POST request to Aylien API
-app.post("/api", getResults);
+app.post('/api',getResults);
+
+
 
 function getResults(req, res) {
-  console.log("I got a request!");
-  console.log(req.body);
-  console.log("inside getResults: ", req.body.url);
-  textapi.sentiment(
-    {
-      url: `${req.body.url}`,
-      mode: "document",
-    },
-    function (error, response) {
-      if (error === null) {
-        console.log(response);
-        res.send(response);
-      } else {
-        console.log("error!", error);
-      }
-    }
-  );
+    textapi.sentiment({
+            url: req.body.url
+        },
+        function(error, response) {
+            res.send(response);
+        }
+    );
 }
